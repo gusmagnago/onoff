86fe3849adeb2169cbc336a54d337b4e20840620
Gustavo Magnago <ext.gustavo.magnago@farfetch.com>
2023-04-25 15:42:41 +0100

    feat: Create select component with fetching shops from json

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, MouseEvent, useState } from 'react';
 import Input from './components/input/Input';
 import ShoppingCartHeader from './components/shopping-cart-header/ShoppingCartHeader';
+import Select from './components/select/Select';
+
+import { useQuery } from 'react-query';
+import { IShop } from './utils/api/types';
+import { fetchShops } from './utils/api';
 
 const App = () => {
   const [inputValue, setInputValue] = useState<string>('');
+  const [open, setOpen] = useState<boolean>(false);
+
+  const [placeholder, setPlaceholder] = useState<string>('Select shop');
+
+  const { data, isLoading } = useQuery<IShop[]>('stores', fetchShops);
+
+  const isInputEmpty =
+    typeof inputValue === 'string' && inputValue.length === 0;
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
+  };
+
+  const handleSelectShop = (e: MouseEvent<HTMLUListElement>) => {
+    setPlaceholder((e.target as HTMLUListElement).innerText);
+    setOpen(!open);
   };
 
   return (
@@ -14,7 +32,18 @@ const App = () => {
       <ShoppingCartHeader
         title={'Add to cart:'}
         children={
-          <Input value={inputValue} onChange={(e) => handleInputChange(e)} />
+          <>
+            <Input value={inputValue} onChange={(e) => handleInputChange(e)} />
+            <Select
+              placeholder={placeholder}
+              open={open}
+              shopNameList={data && data}
+              onClick={() => setOpen(!open)}
+              onSelect={(e) => handleSelectShop(e)}
+              disabled={isInputEmpty}
+              isLoading={isLoading}
+            />
+          </>
         }
       />
     </div>
---
+++ src/utils/api/index.ts
@@ -0,0 +1,7 @@
+import { IShop } from "./types"
+
+export const fetchShops = async (): Promise<IShop[]> => {
+    const response = await fetch('/shops.json');
+    const data = await response.json();
+    return data;
+};
